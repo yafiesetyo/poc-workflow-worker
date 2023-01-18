f91a48b7ab81bec8af08ea63d9475582ae001322
yafiesetyo <muhammad.wibowo@amartha.com>
2023-01-18 16:48:43 +0700

    first commit-salah repo blokk

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	TypeFirstEndpoint  = "first_endpoint"
+	TypeSecondEndpoint = "second_endpoint"
+	TypeThirdEndpoint  = "third_endpoint"
+)
+
+type (
+	CommonPublishReq struct {
+		ID       uint64 `json:"id"`
+		RuleName string `json:"rule_name"`
+		Type     string `json:"type"`
+	}
+)
+
+func WorkflowWorker(ctx context.Context, e []byte) {
+	// unmarshal si data dr pub/sub nya
+	var req CommonPublishReq
+	err := json.Unmarshal(e, &req)
+	if err != nil {
+		logrus.Error("getting error when unmarshal, err: %v", err)
+		return
+	}
+
+	fmt.Println("Incoming request", req)
+
+	// decision utk hit endpoint mana
+	if req.Type == TypeFirstEndpoint {
+		// call ke first endpoint
+		return
+	}
+	if req.Type == TypeSecondEndpoint {
+		// call ke second endpoint
+		return
+	}
+	if req.Type == TypeThirdEndpoint {
+		// call ke third endpoint
+		return
+	}
+}
